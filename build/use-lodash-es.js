078286e697b24b0717896e9a5815330126075f80
Ryan Murphy <rdmurphy@users.noreply.github.com>
2016-04-14 13:30:14 -0500

    Linting, credit to redux

--- build/use-lodash-es.js
+++ build/use-lodash-es.js
@@ -1,7 +1,8 @@
+// borrowed from https://github.com/reactjs/redux/blob/v3.4.0/build/use-lodash-es.js
 module.exports = function () {
   return {
     visitor: {
-      ImportDeclaration(path) {
+      ImportDeclaration (path) {
         var source = path.node.source;
         source.value = source.value.replace(/^lodash($|\/)/, 'lodash-es$1');
       }
