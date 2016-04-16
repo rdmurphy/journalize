4b0f1ddd4903fd57180c1a5b26c48a7fcde4600a
Ryan Murphy <rdmurphy@users.noreply.github.com>
2016-04-15 23:04:17 -0500

    Update README

--- src/postal.js
+++ src/postal.js
@@ -3,10 +3,10 @@ import lookup from './lookup';
 import AP_STATES from './data/states';
 
 /**
- * Converts state names into USPS postal codes, and back. If the supplied
- * string has no match, or if there is a match and the lookup is empty, the
- * original value is returned. If the value is not a string, the original will
- * also be returned.
+ * Converts state and U.S. territory names into USPS postal codes, and back. If
+ * the supplied string has no match, or if there is a match and the lookup is
+ * empty, the original value is returned. If the value is not a string, the
+ * original will also be returned.
  *
  * If `reverse` is true, `postal` will convert a postal code back to a full
  * string.
