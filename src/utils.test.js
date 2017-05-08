f539ab66226b7096bf38d325f1c5ccc60fa26cf1
Ryan Murphy <rdmurphy@users.noreply.github.com>
2017-05-08 08:39:15 -0500

    More util tests

--- src/utils.test.js
+++ src/utils.test.js
@@ -9,8 +9,21 @@ describe('isNil', () => {
     expect(isNil(undefined)).toBe(true);
   });
 
+  it('returns false on other falsey values', () => {
+    expect(isNil(false)).toBe(false);
+    expect(isNil(0)).toBe(false);
+    expect(isNil('')).toBe(false);
+    expect(isNil(NaN)).toBe(false);
+  });
+
   it('returns false on not null/undefined', () => {
     expect(isNil(5)).toBe(false);
+    expect(isNil(5.5)).toBe(false);
+    expect(isNil(Infinity)).toBe(false);
+    expect(isNil(-Infinity)).toBe(false);
+    expect(isNil('hello')).toBe(false);
+    expect(isNil([])).toBe(false);
+    expect(isNil({})).toBe(false);
   });
 });
 
@@ -27,10 +40,30 @@ describe('isString', () => {
 describe('isFinite', () => {
   it('returns true on number', () => {
     expect(_isFinite(5)).toBe(true);
+    expect(_isFinite(5.5)).toBe(true);
+  });
+
+  it('returns true on negative number', () => {
+    expect(_isFinite(-5)).toBe(true);
+    expect(_isFinite(-5.5)).toBe(true);
   });
 
   it('returns false on string', () => {
     expect(_isFinite('5')).toBe(false);
+  });
+
+  it('returns false on undefined', () => {
+    expect(_isFinite(undefined)).toBe(false);
+  });
+
+  it('returns false on null', () => {
+    expect(_isFinite(null)).toBe(false);
+  });
+
+  it('returns false on special numbers', () => {
+    expect(_isFinite(Infinity)).toBe(false);
+    expect(_isFinite(-Infinity)).toBe(false);
+    expect(_isFinite(NaN)).toBe(false);
   });
 });
 
@@ -39,9 +72,21 @@ describe('isInteger', () => {
     expect(isInteger(5)).toBe(true);
   });
 
+  it('returns true on negative integer', () => {
+    expect(isInteger(-5)).toBe(true);
+  });
+
   it('returns false on not integer', () => {
     expect(isInteger('5')).toBe(false);
     expect(isInteger(5.5)).toBe(false);
+    expect(isInteger(undefined)).toBe(false);
+    expect(isInteger(null)).toBe(false);
+
+    it('returns false on special numbers', () => {
+      expect(isInteger(Infinity)).toBe(false);
+      expect(isInteger(-Infinity)).toBe(false);
+      expect(isInteger(NaN)).toBe(false);
+    });
   });
 });
 
