2f81de098f272fd4bb9160b53c12d96321c11284
sergio garcia <sergio.garcia@gmail.com>
2018-10-28 10:27:08 -0400

    Add typings file

---
+++ types/index.d.ts
@@ -0,0 +1,21 @@
+export function apdate(date?: Date): string;
+
+export function apmonth(date?: Date): string;
+
+export function apnumber(val?: number | null): number | string;
+
+export function aptime(date?: Date): string;
+
+export function capfirst(val?: string | null): string;
+
+export function intcomma(val?: string | number | null): string;
+
+export function intword(val?: number): string;
+
+export function ordinal(val?: number): string;
+
+export function pluralize(val: number | string | any[]): string;
+
+export function widont(val?: number, replaceChar?: string): string;
+
+export function yesno(val?: boolean | null, no?: string, yes?: string, maybe?: string): string;
